fc828e11a5ec20cde2c2e1773af620c78ec81109
griselda de lio <griseldadelio@hotmail.com>
2023-11-08 18:51:46 -0300

    elimine errores de consola

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AOS from 'aos';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './screen/Home';
 import { Selection } from './screen/Selection';
@@ -8,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 
 const App = () => {
+  AOS.init();
   return (
     <BrowserRouter>
       <Switch>
